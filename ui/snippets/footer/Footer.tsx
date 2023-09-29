9acbf4d2df3f8d9f9510f8f43161136e171d0dd1
Daniel Marques <daniel@descentralizado.com.br>
2023-09-29 18:32:44 -0300

    feat: remove footer blockscout

--- ui/snippets/footer/Footer.tsx
+++ ui/snippets/footer/Footer.tsx
@@ -1,4 +1,12 @@
-import { Box, Grid, Flex, Text, Link, VStack, Skeleton } from '@chakra-ui/react';
+import {
+  Box,
+  Grid,
+  Flex,
+  Text,
+  Link,
+  VStack,
+  Skeleton,
+} from '@chakra-ui/react';
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
@@ -24,11 +32,10 @@ import getApiVersionUrl from './utils/getApiVersionUrl';
 
 const MAX_LINKS_COLUMNS = 3;
 
-const FRONT_VERSION_URL = `https://github.com/blockscout/frontend/tree/${ config.UI.footer.frontendVersion }`;
-const FRONT_COMMIT_URL = `https://github.com/blockscout/frontend/commit/${ config.UI.footer.frontendCommit }`;
+const FRONT_VERSION_URL = `https://github.com/blockscout/frontend/tree/${config.UI.footer.frontendVersion}`;
+const FRONT_COMMIT_URL = `https://github.com/blockscout/frontend/commit/${config.UI.footer.frontendCommit}`;
 
 const Footer = () => {
-
   const { data: backendVersionData } = useApiQuery('config_backend_version', {
     queryOptions: {
       staleTime: Infinity,
@@ -77,11 +84,19 @@ const Footer = () => {
 
   const frontendLink = (() => {
     if (config.UI.footer.frontendVersion) {
-      return <Link href={ FRONT_VERSION_URL } target="_blank">{ config.UI.footer.frontendVersion }</Link>;
+      return (
+        <Link href={FRONT_VERSION_URL} target='_blank'>
+          {config.UI.footer.frontendVersion}
+        </Link>
+      );
     }
 
     if (config.UI.footer.frontendCommit) {
-      return <Link href={ FRONT_COMMIT_URL } target="_blank">{ config.UI.footer.frontendCommit }</Link>;
+      return (
+        <Link href={FRONT_COMMIT_URL} target='_blank'>
+          {config.UI.footer.frontendCommit}
+        </Link>
+      );
     }
 
     return null;
@@ -89,87 +104,89 @@ const Footer = () => {
 
   const fetch = useFetch();
 
-  const { isLoading, data: linksData } = useQuery<unknown, ResourceError<unknown>, Array<CustomLinksGroup>>(
-    [ 'footer-links' ],
-    async() => fetch(config.UI.footer.links || ''),
-    {
-      enabled: Boolean(config.UI.footer.links),
-      staleTime: Infinity,
-    });
+  const { isLoading, data: linksData } = useQuery<
+    unknown,
+    ResourceError<unknown>,
+    Array<CustomLinksGroup>
+  >(['footer-links'], async () => fetch(config.UI.footer.links || ''), {
+    enabled: Boolean(config.UI.footer.links),
+    staleTime: Infinity,
+  });
 
   return (
     <Flex
       direction={{ base: 'column', lg: 'row' }}
       p={{ base: 4, lg: 9 }}
-      borderTop="1px solid"
-      borderColor="divider"
-      as="footer"
-      columnGap="100px"
+      borderTop='1px solid'
+      borderColor='divider'
+      as='footer'
+      columnGap='100px'
     >
-      <Box flexGrow="1" mb={{ base: 8, lg: 0 }}>
-        <Flex flexWrap="wrap" columnGap={ 8 } rowGap={ 6 }>
-          <ColorModeToggler/>
-          { !config.UI.indexingAlert.isHidden && <IndexingAlertIntTxs/> }
-          <NetworkAddToWallet/>
+      <Box flexGrow='1' mb={{ base: 8, lg: 0 }}>
+        <Flex flexWrap='wrap' columnGap={8} rowGap={6}>
+          <ColorModeToggler />
+          {!config.UI.indexingAlert.isHidden && <IndexingAlertIntTxs />}
+          <NetworkAddToWallet />
         </Flex>
         <Box mt={{ base: 5, lg: '44px' }}>
-          <Link fontSize="xs" href="https://www.blockscout.com">blockscout.com</Link>
+          <Link fontSize='xs' href='https://doric.network/'>
+            Doric Network
+          </Link>
         </Box>
-        <Text mt={ 3 } maxW={{ base: 'unset', lg: '470px' }} fontSize="xs">
-            Blockscout is a tool for inspecting and analyzing EVM based blockchains. Blockchain explorer for Ethereum Networks.
+        <Text mt={3} maxW={{ base: 'unset', lg: '470px' }} fontSize='xs'>
+          Explorer is a tool for inspecting and analyzing EVM based blockchains
         </Text>
-        <VStack spacing={ 1 } mt={ 6 } alignItems="start">
-          { apiVersionUrl && (
-            <Text fontSize="xs">
-                Backend: <Link href={ apiVersionUrl } target="_blank">{ backendVersionData?.backend_version }</Link>
+        <VStack spacing={1} mt={6} alignItems='start'>
+          {apiVersionUrl && (
+            <Text fontSize='xs'>
+              Backend:{' '}
+              <Link href={apiVersionUrl} target='_blank'>
+                {backendVersionData?.backend_version}
+              </Link>
             </Text>
-          ) }
-          { frontendLink && (
-            <Text fontSize="xs">
-              Frontend: { frontendLink }
-            </Text>
-          ) }
+          )}
+          {frontendLink && <Text fontSize='xs'>Frontend: {frontendLink}</Text>}
         </VStack>
       </Box>
       <Grid
         gap={{ base: 6, lg: 12 }}
-        gridTemplateColumns={ config.UI.footer.links ?
-          { base: 'repeat(auto-fill, 160px)', lg: `repeat(${ (linksData?.length || MAX_LINKS_COLUMNS) + 1 }, 160px)` } :
-          'auto'
+        gridTemplateColumns={
+          config.UI.footer.links
+            ? {
+                base: 'repeat(auto-fill, 160px)',
+                lg: `repeat(${
+                  (linksData?.length || MAX_LINKS_COLUMNS) + 1
+                }, 160px)`,
+              }
+            : 'auto'
         }
       >
-        <Box minW="160px" w={ config.UI.footer.links ? '160px' : '100%' }>
-          { config.UI.footer.links && <Text fontWeight={ 500 } mb={ 3 }>Blockscout</Text> }
-          <Grid
-            gap={ 1 }
-            gridTemplateColumns={ config.UI.footer.links ? '160px' : { base: 'repeat(auto-fill, 160px)', lg: 'repeat(3, 160px)' } }
-            gridTemplateRows={{ base: 'auto', lg: config.UI.footer.links ? 'auto' : 'repeat(2, auto)' }}
-            gridAutoFlow={{ base: 'row', lg: config.UI.footer.links ? 'row' : 'column' }}
-            mt={{ base: 0, lg: config.UI.footer.links ? 0 : '100px' }}
-          >
-            { BLOCKSCOUT_LINKS.map(link => <FooterLinkItem { ...link } key={ link.text }/>) }
-          </Grid>
-        </Box>
-        { config.UI.footer.links && isLoading && (
+        {config.UI.footer.links &&
+          isLoading &&
           Array.from(Array(3)).map((i, index) => (
-            <Box minW="160px" key={ index }>
-              <Skeleton w="120px" h="20px" mb={ 6 }/>
-              <VStack spacing={ 5 } alignItems="start" mb={ 2 }>
-                { Array.from(Array(5)).map((i, index) => <Skeleton w="160px" h="14px" key={ index }/>) }
+            <Box minW='160px' key={index}>
+              <Skeleton w='120px' h='20px' mb={6} />
+              <VStack spacing={5} alignItems='start' mb={2}>
+                {Array.from(Array(5)).map((i, index) => (
+                  <Skeleton w='160px' h='14px' key={index} />
+                ))}
               </VStack>
             </Box>
-          ))
-        ) }
-        { config.UI.footer.links && linksData && (
-          linksData.slice(0, MAX_LINKS_COLUMNS).map(linkGroup => (
-            <Box minW="160px" key={ linkGroup.title }>
-              <Text fontWeight={ 500 } mb={ 3 }>{ linkGroup.title }</Text>
-              <VStack spacing={ 1 } alignItems="start">
-                { linkGroup.links.map(link => <FooterLinkItem { ...link } key={ link.text }/>) }
+          ))}
+        {config.UI.footer.links &&
+          linksData &&
+          linksData.slice(0, MAX_LINKS_COLUMNS).map((linkGroup) => (
+            <Box minW='160px' key={linkGroup.title}>
+              <Text fontWeight={500} mb={3}>
+                {linkGroup.title}
+              </Text>
+              <VStack spacing={1} alignItems='start'>
+                {linkGroup.links.map((link) => (
+                  <FooterLinkItem {...link} key={link.text} />
+                ))}
               </VStack>
             </Box>
-          ))
-        ) }
+          ))}
       </Grid>
     </Flex>
   );
