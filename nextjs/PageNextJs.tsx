24913074ce448e6ab449a5a43f373dae892007d8
Daniel Marques <daniel@descentralizado.com.br>
2023-09-29 11:40:38 -0300

    chore: add blockscaunt

---
+++ nextjs/PageNextJs.tsx
@@ -0,0 +1,43 @@
+import Head from 'next/head';
+import React from 'react';
+
+import type { Route } from 'nextjs-routes';
+
+import useAdblockDetect from 'lib/hooks/useAdblockDetect';
+import useConfigSentry from 'lib/hooks/useConfigSentry';
+import useGetCsrfToken from 'lib/hooks/useGetCsrfToken';
+import * as metadata from 'lib/metadata';
+import * as mixpanel from 'lib/mixpanel';
+
+type Props = Route & {
+  children: React.ReactNode;
+}
+
+const PageNextJs = (props: Props) => {
+  const { title, description, opengraph } = metadata.generate(props);
+
+  useGetCsrfToken();
+  useAdblockDetect();
+  useConfigSentry();
+
+  const isMixpanelInited = mixpanel.useInit();
+  mixpanel.useLogPageView(isMixpanelInited);
+
+  return (
+    <>
+      <Head>
+        <title>{ title }</title>
+        <meta name="description" content={ description }/>
+
+        { /* OG TAGS */ }
+        <meta property="og:title" content={ opengraph.title }/>
+        { opengraph.description && <meta property="og:description" content={ opengraph.description }/> }
+        <meta property="og:image" content={ opengraph.imageUrl }/>
+        <meta property="og:type" content="website"/>
+      </Head>
+      { props.children }
+    </>
+  );
+};
+
+export default React.memo(PageNextJs);
